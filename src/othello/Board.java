eedc15fc4444e518c18250965603713feaaea6bb
hari-19 <hari19@outlook.in>
2020-06-10 12:10:11 +0530

    Updated Files

---
+++ src/othello/Board.java
@@ -0,0 +1,35 @@
+package othello;
+
+public class Board {
+    char board[][];
+
+    public Board() {
+        board = new char[][]{
+                {'_', '_', '_', '_', '_', '_', '_', '_',},
+                {'_', '_', '_', '_', '_', '_', '_', '_',},
+                {'_', '_', '_', '_', '_', '_', '_', '_',},
+                {'_', '_', '_', '0', '1', '_', '_', '_',},
+                {'_', '_', '_', '1', '0', '_', '_', '_',},
+                {'_', '_', '_', '_', '_', '_', '_', '_',},
+                {'_', '_', '_', '_', '_', '_', '_', '_',},
+                {'_', '_', '_', '_', '_', '_', '_', '_',}};
+
+    }
+
+    public void printBoard() {
+        System.out.print("  ");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/othello/Main.java
@@ -0,0 +1,10 @@
+package othello;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(":");
+        Board b = new Board();
+        b.printBoard();
+    }
+}
