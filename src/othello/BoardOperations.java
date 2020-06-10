75393d6710c9625b8c67b2b58abcc333cc1bdd13
rohit-pie <rohitnarayanan1729@gmail.com>
2020-06-10 18:41:32 +0530

    Changed parameters of isLegal

--- src/othello/BoardOperations.java
+++ src/othello/BoardOperations.java
@@ -2,12 +2,10 @@ package othello;
 
 class BoardOperations
 {
-    //isLegal function check whether the move is legal and updates the board if a legal move is performed.
-
     static boolean isLegal(Board board,int x,int y, char player, char enemy)
     {   if(isAlreadyMarked(board, x,y))
             return false;
-        else if(isCapturable(board, x,y, player, enemy))
+        else if(isCapturable(board, x,y))
             return true;                  
 
         return false;
@@ -19,148 +17,127 @@ class BoardOperations
         return true;
     }
 
-    static boolean isCapturable(Board board, int x,int y, char player, char enemy)
+    static boolean isCapturable(Board board, int x,int y)
     {
-        return updatePieces(board,x,y,player,enemy);
+        return true;
     }
 
-    static boolean updatePieces(Board board,int x, int y, char player, char enemy)
+
+    //Once we get a legal move, we perform updatePieces() function. But how to find a legal move? 
+    void updatePieces(Board board,int x, int y, char player, char enemy)
     {
         int i=x,j=y;
-        board.board[i][j]=player;
-        boolean isCapturable = false;
+        board.board[i][j]=player; 
               
         //CHECKING ELEMENT AND UPDATING ALONG NORTH-WEST DIRECTION
-        if(i-1>=0&&j-1>=0&&board.board[i-1][j-1]==enemy)
+        if(board.board[i-1][j-1]==enemy&&i-1>=0&&j-1>=0)
         {   i--;j--;
-            while(i>0&&j>0&&board.board[i][j]==enemy)
+            while(board.board[i][j]==enemy&&i>0&&j>0)
             {   i--;
                 j--;
             }
-            if(i>=0&&j>=0&&board.board[i][j]==player)
-            {   if(i<x)
-                    isCapturable = true;
-                while(i!=x-1 && j!=y-1)
+            if(board.board[i][j]==player&&i>=0&&j>=0)
+            {   while(i!=x-1 && j!=y-1)
                     board.board[++i][++j]=player;
             }
         } 
         
         //CHECKING ELEMENT AND UPDATING ALONG NORTH DIRECTION 
         i=x;j=y; //Reassigning variables since i and j might have changed in the first block of updating.
-        if(j-1>=0&&board.board[i][j-1]==enemy)
+        if(board.board[i][j-1]==enemy&&j-1>=0)
         {   j--;
             while(board.board[i][j]==enemy&&j>0)
             {    j--;
             }
-            if(j>=0&&board.board[i][j]==player)
-            {   if(j<y)
-                    isCapturable = true;
-                while(j!=y-1)
+            if(board.board[i][j]==player&&j>=0) 
+            {   while(j!=y-1)
                     board.board[i][++j]=player;
             }
         }
 
         //CHECKING ELEMENT AND UPDATING ALONG NORTH-EAST DIRECTION
         i=x;j=y; 
-        if(i+1<=7&&j-1>=0 && board.board[i+1][j-1]==enemy)
+        if(board.board[i+1][j-1]==enemy&&i+1<=7&&j-1>=0)
         {   i++;
             j--;
-            while(i<7&&j>0 && board.board[i][j]==enemy)
+            while(board.board[i][j]==enemy&&i<7&&j>0)
             {   i++;
                 j--;
             }
-            if(i<=7&&j>=0 && board.board[i][j]==player)
-            {   if(j<y)
-                    isCapturable=true;
-                while(i!=x+1&&j!=y-1)
+            if(board.board[i][j]==player&&i<=7&&j>=0) 
+            {   while(i!=x+1&&j!=y-1)
                     board.board[--i][++j] = player;
             }
         }
 
         //CHECKING ELEMENT AND UPDATING ALONG WEST DIRECTION
         i=x;j=y; 
-        if(i-1>=0 && board.board[i-1][j]==enemy)
+        if(board.board[i-1][j]==enemy&&i-1>=0)
         {   i--;
-            while(i>0 && board.board[i][j]==enemy) 
+            while(board.board[i][j]==enemy&&i>0) 
             {    i--;
             }
-            if(i>=0 && board.board[i][j]==player)
-            {   if(i<x)
-                    isCapturable = true;
-                while(i!=x-1)
+            if(board.board[i][j]==player&&i>=0) 
+            {   while(i!=x-1)
                     board.board[++i][j]=player;
             }
         } 
 
         //CHECKING ELEMENT AND UPDATING ACROSS EAST DIRECTION
         i=x;j=y; 
-        if(i+1<=7 && board.board[i+1][j]==enemy)
+        if(board.board[i+1][j]==enemy&&i+1<=7)
         {   i++;
-            while(i<7 && board.board[i][j]==enemy) 
+            while(board.board[i][j]==enemy&&i<7) 
             {    i++;
             }
-            if(i<=7 && board.board[i][j]==player) 
-            {   if(i>x)
-                    isCapturable = true;
-                while(i!=x+1)
-                    board.board[--i][j]=player;
-            }
+            if(board.board[i][j]==player&&i<=7) 
+            {   while(i!=x+1)
+                    board.board[--i][j]=player;}
         }
         
         //CHECKING ELEMENT AND UPDATING ALONG SOUTH-WEST DIRECTION
         i=x;j=y; 
-        if(i-1>=0&&j+1<=7 && board.board[i-1][j+1]==enemy)
+        if(board.board[i-1][j+1]==enemy&&i-1>=0&&j+1<=7)
         {   i--;
             j++;
-            while(i>0&&j<7 && board.board[i][j]==enemy)
+            while(board.board[i][j]==enemy&&i>0&&j<7)
             {   i--;
                 j++;
             }
-            if(i>=0&&j<=7 && board.board[i][j]==player)
-            {   if(i<x)
-                    isCapturable = true;
-                while(i!=x-1 && j!=y+1)
+            if(board.board[i][j]==player&&i>=0&&j<=7)
+            {   while(i!=x-1 && j!=y+1)
                      board.board[++i][--j]=player;
             }
         }   
 
         //CHECKING ELEMENT AND UPDATING ALONG SOUTH DIRECTION        
         i=x;j=y; 
-        if(j+1<=7 && board.board[i][j+1]==enemy)
+        if(board.board[i][j+1]==enemy&&j+1<=7)
         {  j++;
-            while(j<7 && board.board[i][j]==enemy)
+            while(board.board[i][j]==enemy&&j<7)
             {   j++;
             }
-            if(j<=7 && board.board[i][j]==player) 
-            {   if(j>y)
-                    isCapturable = true;
-
-                while(j!=y+1)
+            if(board.board[i][j]==player&&j<=7) 
+            {   while(j!=y+1)
                     board.board[i][--j] = player;
             }
         }
 
         //CHECKING ELEMENT AND UPDATING ALONG SOUTH-EAST DIRECTION
         i=x;j=y;
-        if(i+1<=7&&j+1<=7 && board.board[i+1][j+1]==enemy)
+        if(board.board[i+1][j+1]==enemy&&i+1<=7&&j+1<=7)
         {   i++;
             j++;
-            while(i<7&&j<7 && board.board[i][j]==enemy)
+            while(board.board[i][j]==enemy&&i<7&&j<7)
             {   i++;
                 j++;
             }
-            if(i<=7&&j<=7 && board.board[i][j]==player)
-            {   if(i>x)
-                    isCapturable = true;
-                while(i!=x+1&&j!=y+1)
+            if(board.board[i][j]==player&&i<=7&&j<=7)
+            {   while(i!=x+1&&j!=y+1) 
                     board.board[--i][--j]=player;
             }
         }
-
-        if(!isCapturable)
-            board.board[x][y]='_';
-
-        return isCapturable;
+ 
        //Change occurs in both variables i and j only in diagonal directions
     }  
     
